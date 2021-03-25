2236f06ffdca4006462ba13e1511d01a7eaf7ef7
LAPTOP-9QCCO3IF\Pierre <pmalard1998@gmail.com>
2021-03-25 10:39:15 +0100

    lib

--- CATS_SERVER/venv/Scripts/Activate.ps1
+++ CATS_SERVER/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\jelly\PycharmProjects\CATS_SERVER\venv"
+$env:VIRTUAL_ENV="C:\Users\Pierre\Documents\SERVERKITTY\CATS_SERVER\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
